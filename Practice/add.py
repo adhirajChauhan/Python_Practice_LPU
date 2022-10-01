186bf8cc5c0f1ea8c47c6897cb10415074afc9ca
adhirajChauhan <adhirajsingh0105@gmail.com>
2022-10-01 15:38:42 +0530

    add program

---
+++ Practice/add.py
@@ -0,0 +1,9 @@
+#Create two variables (x and y) and assign values to them
+
+#create a third variable(sum) and add x and y
+
+#print the sum
+x = 2
+y = 5
+sum = x+y
+print("The answer is : " , sum)
